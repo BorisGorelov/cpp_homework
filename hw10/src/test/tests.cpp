277e952a8460ab4b2c4ffbfbd5d993d3f28c2cdc
boris <borisgorelovspb@gmail.com>
2019-12-06 17:39:06 +0300

    added rotation to different angles

--- hw10/src/test/tests.cpp
+++ hw10/src/test/tests.cpp
@@ -8,10 +8,10 @@ TEST(turn_left_test, test1){
     std::pair<float, float> three (1., 0.);
     std::pair<float, float> four (0., 1.);
     std::pair<float, float> five (-2., 1.);
-    std::pair<float, float> six (-1., -2.);
+    std::pair<float, float> six (2., -1.);
     std::pair<float, float> seven (0., 0.);
-    EXPECT_EQ(turn_left(one), two);
-    EXPECT_EQ(turn_left(three), four);
-    EXPECT_EQ(turn_left(five), six);
-    EXPECT_EQ(turn_left(seven), seven);
+    EXPECT_EQ(turn(one, 90), two);
+    EXPECT_ANY_THROW(turn(three, 91));
+    EXPECT_EQ(turn(five, 180), six);
+    EXPECT_EQ(turn(seven, 270), seven);
 }
